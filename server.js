fbb5ab8e5d3b90a53c359d016c648f4cc935f5a0
Anton Syzov <aantonssizov@gmail.com>
2026-05-10 00:46:32 +0200

    fix port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 7500;
 const contactsRoute = require("./routes/contactsRoute");
 const db = require("./database/connection");
 
